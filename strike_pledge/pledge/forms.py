ff42bd1ef31e8428d325e8d0369d44b70fac21cc
Worthington <morgan.worthington@capgemini.com>
2019-05-24 21:43:22 -0700

    removed faq page, modified the form page

--- strike_pledge/pledge/forms.py
+++ strike_pledge/pledge/forms.py
@@ -7,15 +7,9 @@ class PledgeForm(forms.Form):
 class ValidateForm(forms.Form):
     email_hash = forms.CharField(max_length=200, widget=forms.HiddenInput(), label='')
     work_email = forms.CharField(max_length=200, widget=forms.HiddenInput(), label='')
-    union_list = (
-        ('none', 'Union / Non-union'),
-        ('yes', 'Union'),
-        ('no', 'Non-union')
-    )
-    union_member = forms.ChoiceField(choices=union_list, required=False, label='')
     personal_email=forms.EmailField(required=False, label='', initial='', widget=forms.TextInput(attrs={'placeholder':'Personal email (optional)'}))
     region_list = (
-        ('none', 'KP Region'),
+        ('none', 'Kaiser Region'),
         ('norcal', 'Northern California'),
         ('socal', 'Southern California'),
         ('colorado', 'Colorado'),
@@ -28,4 +22,10 @@ class ValidateForm(forms.Form):
         ('washington', 'Washington State')
     )
     kaiser_region = forms.ChoiceField(choices=region_list, required=False, label='')
+    union_list = (
+        ('none', 'SEIU-UHW member?'),
+        ('yes', 'Yes'),
+        ('no', 'No (sympathy striker)')
+    )
+    union_member = forms.ChoiceField(choices=union_list, required=False, label='')
     tweet = forms.CharField(max_length=280, required=False, initial='', label='', widget=forms.Textarea(attrs={'placeholder':'Why are you going on strike? (optional)'}))
--- strike_pledge/pledge/urls.py
+++ strike_pledge/pledge/urls.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import emailView, successView, validateView, aboutView, faqView, termsView, privacyView
+from .views import emailView, successView, validateView, aboutView, termsView, privacyView
 
 urlpatterns = [
     path('email/', emailView, name='email'),
     path('success/', successView, name='success'),
     path('validate/', validateView, name='validate'),
 	path('about/', aboutView, name='about'),
-    path('faq/', faqView, name='faq'),
     path('terms/', termsView, name='terms'),
 	path('privacy/', privacyView, name='privacy'),
 ]
