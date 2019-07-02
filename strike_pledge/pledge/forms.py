37f3eddb612da9e3e6561a136d97a875dc1b9ebd
project contribute <projectcontribute@maskmemail.com>
2019-07-01 23:15:01 -0700

    Allow any domain for referral emails

--- strike_pledge/pledge/forms.py
+++ strike_pledge/pledge/forms.py
@@ -31,8 +31,8 @@ class ValidateForm(forms.Form):
     personal_email=forms.EmailField(required=False, label='', initial='', widget=forms.TextInput(attrs={'placeholder':'Personal email (optional)'}))
 
 class ReferralForm(forms.Form):
-    email1 = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'first.(initial).last','size':22}))
-    email2 = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'first.(initial).last','size':22}))
-    email3 = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'first.(initial).last','size':22}))
-    email4 = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'first.(initial).last','size':22}))
-    email5 = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'first.(initial).last','size':22}))
+    email1 = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'janedoe@email.com','size':22}))
+    email2 = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'johndoe@email.com','size':22}))
+    email3 = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'janedoe@email.com','size':22}))
+    email4 = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'johndoe@email.com','size':22}))
+    email5 = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'janedoe@email.com','size':22}))
--- strike_pledge/pledge/views.py
+++ strike_pledge/pledge/views.py
@@ -108,13 +108,13 @@ def confirmView(request):
     else:
         form = ReferralForm(request.POST)
         if form.is_valid():
-            email1 = form.cleaned_data['email1'] + '@kp.org'
-            email2 = form.cleaned_data['email2'] + '@kp.org'
-            email3 = form.cleaned_data['email3'] + '@kp.org'
-            email4 = form.cleaned_data['email4'] + '@kp.org'
-            email5 = form.cleaned_data['email5'] + '@kp.org'
+            email1 = form.cleaned_data['email1']
+            email2 = form.cleaned_data['email2']
+            email3 = form.cleaned_data['email3']
+            email4 = form.cleaned_data['email4']
+            email5 = form.cleaned_data['email5']
             subject = 'Make a digital strike pledge'
-            message = 'Hello!\n\nYou or your co-worker indicated you want to join the Oct/Nov 2019 Kaiser strike.\n\n'
+            message = 'Hello!\n\nYour co-worker indicated you want to join the Oct/Nov 2019 Kaiser strike.\n\n'
             message += 'Click on this link to make a digital strike pledge and to tweet using our handle (@kaiserstrike19):\n\n'
             message += 'https://kaiserstrike.org'
             message += '\n\n\n\n\nFrom,\n\n'
