0d531e016abbd18c59c85940a1698613275b2b33
project contribute <projectcontribute@maskmemail.com>
2019-07-08 00:37:57 -0700

    Revert "DO NOT DEPLOY W/O REVIEW - Update to contact.html and forms.py"
    This reverts commit 9fc33bc509a400bdf8a08a7d7c3ad9f1c2dba29d.

--- strike_pledge/pledge/forms.py
+++ strike_pledge/pledge/forms.py
@@ -8,7 +8,7 @@ class ValidateForm(forms.Form):
     email_hash = forms.CharField(max_length=200, widget=forms.HiddenInput(), label='')
     work_email = forms.CharField(max_length=200, widget=forms.HiddenInput(), label='')
     region_list = (
-        ('none', '-----'),
+        ('none', 'Kaiser Region'),
         ('norcal', 'California - Northern'),
         ('socal', 'California - Southern'),
         ('colorado', 'Colorado'),
@@ -22,13 +22,13 @@ class ValidateForm(forms.Form):
     )
     kaiser_region = forms.ChoiceField(choices=region_list, required=True, label='')
     union_list = (
-        ('none', '-----'),
+        ('none', 'Member of a Coalition union?'),
         ('yes', 'Yes'),
         ('no', 'No (sympathy striker)')
     )
     union_member = forms.ChoiceField(choices=union_list, required=True, label='')
-    tweet = forms.CharField(max_length=235, required=False, initial='', label='', widget=forms.Textarea(attrs={'placeholder':'Your comment will tweeted (anonymously)'}))
-    personal_email=forms.EmailField(required=False, label='', initial='', widget=forms.TextInput(attrs={'placeholder':'janedoe@email.com'}))
+    tweet = forms.CharField(max_length=235, required=False, initial='', label='', widget=forms.Textarea(attrs={'placeholder':'Why are you making a strike pledge? (optional)'}))
+    personal_email=forms.EmailField(required=False, label='', initial='', widget=forms.TextInput(attrs={'placeholder':'Personal email (optional)'}))
 
 class ReferralForm(forms.Form):
     email1 = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'first.(initial).last','size':22}))
