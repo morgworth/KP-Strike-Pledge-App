9d94b2e9a65f97587dda4e16839a3259c481e823
project contribute <projectcontribute@maskmemail.com>
2019-07-07 20:51:49 -0700

    Change personal email placeholders

--- strike_pledge/pledge/forms.py
+++ strike_pledge/pledge/forms.py
@@ -32,9 +32,9 @@ class ValidateForm(forms.Form):
 
 class ReferralForm(forms.Form):
     email1 = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'janedoe@email.com','size':22}))
-    email2 = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'johndoe@email.com','size':22}))
+    email2 = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'janedoe@email.com','size':22}))
     email3 = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'janedoe@email.com','size':22}))
-    email4 = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'johndoe@email.com','size':22}))
+    email4 = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'janedoe@email.com','size':22}))
     email5 = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'janedoe@email.com','size':22}))
 
 class HelpForm(forms.Form):
