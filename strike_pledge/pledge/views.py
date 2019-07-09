0d1865d17c5bbeaffc486fe5c045289052f7a3fb
project contribute <projectcontribute@maskmemail.com>
2019-07-08 22:37:12 -0700

    Update confirmation pages

--- strike_pledge/pledge/views.py
+++ strike_pledge/pledge/views.py
@@ -164,11 +164,11 @@ def confirm2View(request):
     else:
         form = ReferralForm(request.POST)
         if form.is_valid():
-            email1 = form.cleaned_data['email1'] + '@kp.org'
-            email2 = form.cleaned_data['email2'] + '@kp.org'
-            email3 = form.cleaned_data['email3'] + '@kp.org'
-            email4 = form.cleaned_data['email4'] + '@kp.org'
-            email5 = form.cleaned_data['email5'] + '@kp.org'
+            email1 = form.cleaned_data['email1']
+            email2 = form.cleaned_data['email2']
+            email3 = form.cleaned_data['email3']
+            email4 = form.cleaned_data['email4']
+            email5 = form.cleaned_data['email5']
             subject = 'Make a digital strike pledge'
             message = 'Hello!\n\nYour co-worker indicated you want to join the Oct/Nov 2019 Kaiser strike.\n\n'
             message += 'Visit this website to make a digital strike pledge and to post a public tweet. Anonymously.\n\n'
