c7ccdb3d15a3f6724a89ac367b3786839efbd940
project contribute <projectcontribute@maskmemail.com>
2019-07-15 22:39:44 -0700

    Add referral form to success page

--- strike_pledge/pledge/urls.py
+++ strike_pledge/pledge/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import emailView, successView, validateView, aboutView, termsView, privacyView, faqView, homeView, confirmView, confirm2View, unionView, helpView, helpsuccessView
+from .views import emailView, successView, success2View, validateView, aboutView, termsView, privacyView, faqView, homeView, confirmView, confirm2View, unionView, helpView, helpsuccessView
 
 urlpatterns = [
     path('email/', emailView, name='email'),
     path('success/', successView, name='success'),
+    path('success2/', success2View, name='success2'),
     path('validate/', validateView, name='validate'),
 	path('about/', aboutView, name='about'),
     path('terms/', termsView, name='terms'),
