352fbcdea7faa04fa4ba351140eed6f2f339a577
project contribute <projectcontribute@maskmemail.com>
2019-09-07 11:44:16 -0700

    Update urls.py

--- strike_pledge/pledge/urls.py
+++ strike_pledge/pledge/urls.py
@@ -3,14 +3,12 @@ from django.urls import path
 
 from .views import successView, validateView, termsView, privacyView, homeView, confirmView, unionView, secretView
 
-
 urlpatterns = [
     path('success/', successView, name='success'),
     path('validate/', validateView, name='validate'),
     path('terms/', termsView, name='terms'),
-	path('privacy/', privacyView, name='privacy'),
-	path('', homeView, name='home'),
-	path('confirm/', confirmView, name='confirm'),
-	path('union/', unionView, name='union'),
-    path('secret/', secretView, name='secret')
-]
+    path('privacy/', privacyView, name='privacy'),
+    path('', homeView, name='home'),
+    path('confirm/', confirmView, name='confirm'),
+    path('union/', unionView, name='union'),
+    path('secret/', secretView, name='secret')]
